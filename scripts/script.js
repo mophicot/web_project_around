d525c271dac661739dd4d89bd9e07e71e7666d3e
Sergio Rangel Ordoñez <srangelor@gmail.com>
2025-04-17 16:41:43 -0600

    Ver 0.0 html page e index básico icon <3

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+// El evento especial submit envía el formulario al servidor
+//  (aún no lo has estudiado, pero no te preocupes).
+//   Mira cuidadosamente el ejemplo de código para manejar este evento,
+//   y trata de comprender lo que está pasando.
+//   Hemos añadido algunos comentarios en el código para ayudarte:
+
+// Busquemos el formulario en el DOM
+// let formElement = // Utiliza el método querySelector()
+
+// Lo siguiente es el manipulador (handler) de entrega de formularios, aunque
+// no se enviará en ningún sitio todavía
+
+// Observa que el nombre de la función comienza con un verbo
+// y describe exactamente lo que hace la función
+// function handleProfileFormSubmit(evt) {
+// Esta línea impide que el navegador
+// entregue el formulario en su forma predeterminada.
+// evt.preventDefault();
+// Una vez hecho esto, podemos definir nuestra propia forma de entregar el formulario.
+// Lo explicaremos todo con más detalle después.
+
+// Busquemos los campos del formulario en el DOM
+// let nameInput = // Utiliza el método querySelector()
+// let jobInput = // Utiliza el método querySelector()
+
+// Obtén los valores de cada campo desde la propiedad de valor correspondiente
+
+// Selecciona los elementos donde se introducirán los valores de los campos
+
+// Inserta nuevos valores utilizando el textContent
+// propiedad del método querySelector()
+// }
+
+// Conecta el manipulador (handler) al formulario:
+// se observará el evento de entrega
+// formElement.addEventListener('submit', handleProfileFormSubmit);
